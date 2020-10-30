040a7de578046df93d9a4fa9746dfe94b7d9d669
yehorovvlad <vladzo1996@gmail.com>
2020-10-30 13:25:47 +0200

    Add header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('searchInput') searchInput: ElementRef;
+  searchIsOpen: boolean;
+  menuIsOpen: boolean;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  toggleSearch() {
+    this.searchIsOpen = !this.searchIsOpen;
+    if(this.searchIsOpen && this.searchInput) {
+      this.searchInput.nativeElement.focus();
+    }
   }
 
+  toggleMenu() {
+    this.menuIsOpen = !this.menuIsOpen;
+  }
 }
